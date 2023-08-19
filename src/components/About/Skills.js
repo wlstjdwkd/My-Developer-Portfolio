db7f16defb6ed3dfe9b1c87f4bd0d33377ada601
wlstjdwkd <xkakrlfh@naver.com>
2023-08-20 00:55:45 +0900

    skills 등록

---
+++ src/components/About/Skills.js
@@ -0,0 +1,10 @@
+function Skills() {
+  //TODO: skills 아이콘 배치해서 귀엽게 about.js에 두기
+  const front_end = {};
+
+  const back_end = {};
+
+  const infra = {};
+  return <></>;
+}
+export default Skills;
