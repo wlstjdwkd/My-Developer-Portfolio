73159c8e3c91727110c2ce3c71e4739e0aa2dc23
wlstjdwkd <xkakrlfh@naver.com>
2023-08-20 00:41:29 +0900

    home 화면

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,37 @@
-import { Button, Typography, Container } from "@mui/material";
+import React from "react";
+import { Button, Typography, Container, Grid } from "@mui/material";
 import { styled } from "@mui/system";
+import profileImage from "../assets/images/umbrella.jpg";
+
 const StyledContainer = styled(Container)({
   textAlign: "center",
   paddingTop: "40px",
   paddingBottom: "40px",
 });
 
-const StyledTypography = styled(Typography)({
-  marginBottom: "20px",
+const StyledImage = styled("img")({
+  width: "100%",
+  maxWidth: "400px", // 원하는 최대 너비를 설정할 수 있습니다.
+  height: "auto",
+  borderRadius: "5px", // 약간의 모서리를 둥글게 하려면 값을 조정할 수 있습니다.
 });
+
 function Home() {
   return (
-    <div>
-      <StyledContainer>
-        <StyledTypography variant="h2" gutterBottom>
+    <Grid container spacing={4} alignItems="center">
+      <Grid item xs={12} md={6}>
+        <Typography variant="h4" gutterBottom>
           안녕하세요, 방진성입니다.
-        </StyledTypography>
-        <StyledTypography variant="h6">
+        </Typography>
+        <Typography variant="subtitle1">
           커뮤니케이션 전문 개발자로서 다양한 프로젝트 경험을 바탕으로 팀원들과
           원활하게 협업하며 목표를 효과적으로 달성하고자 합니다.
-        </StyledTypography>
-        <Button variant="contained" color="primary">
-          포트폴리오 보기
-        </Button>
-      </StyledContainer>
-    </div>
+        </Typography>
+      </Grid>
+      <Grid item xs={12} md={6} container justifyContent="flex-end">
+        <StyledImage src={profileImage} alt="방진성 프로필 이미지" />
+      </Grid>
+    </Grid>
   );
 }
 
